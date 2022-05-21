737f450e6eb79a278061463bf3ec21ef63745923
mirkobosetti <mirkobosetti1998@gmail.com>
2022-05-21 09:34:32 +0200

    network visualizer

--- src/main.js
+++ src/main.js
@@ -1,27 +1,36 @@
-const canvas = document.getElementById('mainCanvas')
+const mainCanvas = document.getElementById('mainCanvas')
+const networkCanvas = document.getElementById('networkCanvas')
 
-canvas.width = 200
+mainCanvas.width = 200
+networkCanvas.width = 500
 
-const ctx = canvas.getContext('2d')
-const road = new Road(canvas.width / 2, canvas.width * 0.9)
-const car = new Car(road.getLaneCenter(1), 100, 30, 50, "KEYS", 2)
+const mainCtx = mainCanvas.getContext('2d')
+const networkCtx = networkCanvas.getContext('2d')
+
+const road = new Road(mainCanvas.width / 2, mainCanvas.width * 0.9)
+const car = new Car(road.getLaneCenter(1), 100, 30, 50, "AI", 2)
 
 const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY")]
 
 animate()
 
-function animate() {
+function animate(time) {
 	for (let i = 0; i < traffic.length; i++) traffic[i].update(road.borders, [])
 
 	car.update(road.borders, traffic)
 
-	canvas.height = window.innerHeight
+	mainCanvas.height = window.innerHeight
+	networkCanvas.height = window.innerHeight
 
-	ctx.save()
-	ctx.translate(0, -car.y + canvas.height * 0.7)
-	road.draw(ctx)
-	for (let i = 0; i < traffic.length; i++) traffic[i].draw(ctx, "black")
-	car.draw(ctx, "blue")
-	ctx.restore()
+	mainCtx.save()
+	mainCtx.translate(0, -car.y + mainCanvas.height * 0.7)
+	road.draw(mainCtx)
+	for (let i = 0; i < traffic.length; i++) traffic[i].draw(mainCtx, "black")
+	car.draw(mainCtx, "blue")
+	mainCtx.restore()
+
+	networkCtx.lineDashOffset = -time/50
+	Visualizer.drawNetwork(networkCtx, car.brain)
+
 	requestAnimationFrame(animate)
 }
